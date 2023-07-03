5c6179cdb4ef021d06564c4a1c8004c1966eeeea
Georgiy Garenov <g_garenov@outlook.com>
2023-07-03 21:40:08 +0300

    doms

--- DOM_Manipulations/02. Delete-From-Table/app.js
+++ DOM_Manipulations/02. Delete-From-Table/app.js
@@ -1,12 +1,3 @@
 function deleteByEmail() {
-  let email = document.getElementsByName("email")[0].value;
-  let secondColumn = document.querySelectorAll("#customers tr th:nth-child(2)");
-  for (let td of secondColumn) {
-    if (td.textContent == email) {
-      let row = td.parentNode;
-      row.parentNode.removeChild(row);
-      document.getElementById("result").textContent = "Deleted";
-      return;
-    }
-  }
+
 }
