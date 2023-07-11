8f34cefdcb17994012bad6df3937a737e8020b27
Georgiy Garenov <g_garenov@outlook.com>
2023-07-11 17:13:57 +0300

    Unit_Testing

---
+++ UnitTestingAndErrorHandling/04sum.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+
+function sum(arr) {
+    let sum = 0;
+    for (let num of arr) {
+        sum += Number(num);
+    }
+    return sum
+}
+
+describe('sum function', function () {
+    it('should start with 0 initial sum')
+})
