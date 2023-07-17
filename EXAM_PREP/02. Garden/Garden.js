8668a1c64b276cc49bad4ed878aeac3f790bffa6
Georgiy Garenov <g_garenov@outlook.com>
2023-07-17 21:31:00 +0300

    Tasks (exam prep)

--- EXAM_PREP/02. Garden/Garden.js
+++ EXAM_PREP/02. Garden/Garden.js
@@ -1,3 +1,9 @@
 class Garden {
 
+    constructor(spaceAvailable, plants, storage) {
+        this.space = spaceAvailable;
+        this.plants = plants;
+        this.storage = storage;
+    }
+
 }
